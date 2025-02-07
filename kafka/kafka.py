20408b5564671da660e876908fe79c8cf6b3520e
gopipandit <gopi1035@gmail.com>
2025-02-08 00:11:12 +0530

    Updated Kafka Code

--- kafka/kafka.py
+++ kafka/kafka.py
@@ -8,7 +8,7 @@ load_dotenv("F:\Pyspark-2025\pyspark\MongoDB\.env")
 
 bootstrap_server = os.getenv("bootstrap_server")
 api_key = os.getenv("api_key")
-api_secret = os.getenv("bootstrap_server")
+api_secret = os.getenv("api_secret")
 
 # Configuration
 conf = {
@@ -23,20 +23,20 @@ conf = {
 producer = Producer(conf)
 
 
-# topic = 'gopinath-de'
+topic = 'gopinath-de'
 
-# def delivery_callback(err, msg):
-#     if err:
-#         print(f'Message delivery failed: {err}')
-#     else:
-#         print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
+def delivery_callback(err, msg):
+    if err:
+        print(f'Message delivery failed: {err}')
+    else:
+        print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
-# # for i in range(1, 11):
-# #     data = {"index": i, "message": f"Hello Kafka! From Gopi Nath- {i}"}
-# #     producer.produce(topic, key=str(i), value=json.dumps(data))
-# # producer.flush()
-# # print("All messages sent to Kafka")
+for i in range(1, 11):
+    data = {"index": i, "message": f"Hello Kafka! From Gopi Nath- {i}"}
+    producer.produce(topic, key=str(i), value=json.dumps(data),callback=delivery_callback)
+producer.flush()
+print("All messages sent to Kafka")
 
 # producer.produce(topic, key='test_key', value='test_value', callback=delivery_callback)
-# producer.flush()
-# print("Test message sent to Kafka")
+producer.flush()
+print("Test message sent to Kafka")
